16a0766e3e0bd72b6cd2148140ec8dd748bf8829
shadykoura <58347770+shadykoura@users.noreply.github.com>
2019-12-14 12:41:09 +0200

    Update NL_to_pyspark_initial _code1.py

--- NL_to_pyspark_initial _code1.py
+++ NL_to_pyspark_initial _code1.py
@@ -8,27 +8,31 @@ from nltk.corpus import wordnet
 import pandas as pd
 from itertools import product
 from fuzzywuzzy import fuzz
-
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 import pycountry
 import nltk
 import re
+from dateutil.parser import parse
+from dateparser.search import search_dates
+
 
 ##input inquiry 
-example_sent = "i want orders before date 2002  "
+example_sent = "i need the position of all players "
+
+example_sent=example_sent.lower()
 ## configuring dictionary for the dataframes available with their attributes 
-DF_att={"employee":["salary","employee names"],"customer":["customer ID","orders","order_id","author","customer names","Date"],"players":["player","rnd","position","nationality","team","pick","school"]}
+DF_att={"employee":["salary","employee names","department","employee id","manager"],"customer":["customer id","orders","order_id","author","customer names","year","book"],"players":["player name","round","position","nationality","team","pick","school"]}
 ##configuring mapping Dictionaries and dataframes
 mapping_DF = pd.DataFrame(columns=['Select', 'AGG', 'Where',"operator"])
-mapping_Dic={"Select":[],"AGG":[],"Where":[],"operator":[],"op_values":[]}
+mapping_Dic={"Select":[],"AGG":[],"Where":[],"operator":[],"op_values":[],"between":[]}
 
 ## identifying the select statment and removing its input words from the sentence 
 
 SELECT_Dict={}
 
-#extracting select 
-select_list=["look for","what is","give","show","choose","return","look","bring","search","what","what's","what is","find","get","who","want"]
+#extracting select pointers and removing them from sentence
+select_list=["calculate","i have","extract","select","look for","what is","give","show","choose","return","look","bring","search","what","what's","what is","find","get","who","want"]
 for i in select_list:
     if i in example_sent:
         example_sent = re.sub(i, '', example_sent)
@@ -38,11 +42,21 @@ for i in select_list:
 Select_sent=example_sent
 ##tokenizing the inquiry 
 word_tokens = word_tokenize(example_sent) 
+
+
+# solving ID issue by concating ID to the previous word so as to have best match with our attribute 
+for f in range(len(word_tokens)):
+    if "ID" == word_tokens[f] or "id" == word_tokens[f] :
+        x=word_tokens[f-1] +word_tokens[f]
+        example_sent = re.sub(word_tokens[f-1] +" " + word_tokens[f], x, example_sent)
+        
+word_tokens = word_tokenize(example_sent) 
+    
 word_tokens_select = word_tokenize(example_sent) 
 list_attributes_matched=[]
 where_exist=False
-#splitting the sentenance before and after the where clause    
-Where_list=["of","which","for","from","with","between","lower than","greater than","more than","less than","before","after","at","where","in","When"]
+#splitting the sentenance into 2 sentences select and where clauses if exist and retokenize them  
+Where_list=["have","has","having","during","which","for","from","with","between","lower than","greater than","more than","less than","before","after","at","where","in","When"]
 
 for i in range(len(word_tokens)):
     if word_tokens[i] in Where_list:
@@ -78,12 +92,14 @@ stop_words.discard("more")
 stop_words.discard("below") 
 stop_words.discard("between") 
 stop_words.add("what is ") 
+stop_words.add(",") 
+stop_words.add("done")
 
 
 
  
 
-########## tokenizing and filtering select Clause 
+########## filtering select Clause after removing stop words
 
   
 filtered_sentence_select = [] 
@@ -95,7 +111,7 @@ for w in word_tokens_select:
 print(word_tokens_select) 
 print(filtered_sentence_select) 
 
-
+Df_name=[]
 
 ########## tokenizing and filtering Where Clause 
 if where_exist:
@@ -138,7 +154,7 @@ if where_exist:
            Partial_Ratio = fuzz.partial_ratio(word1.lower(),word2.lower())
            Token_Sort_Ratio = fuzz.token_sort_ratio(word1,word2)
            Token_Set_Ratio = fuzz.token_set_ratio(word1,word2)
-           if Ratio >= 80:
+           if Ratio >= 65:
                print(  word1 +" matches with attribute  "+ word2+" with ratio " + str(Ratio) + " in the where clause ")
                list_attributes_matched.append(word2)
                ID_list=[" ID ","_ID"]
@@ -146,6 +162,10 @@ if where_exist:
                for d in ID_list: 
                    if d in word2 and d not in word1:
                       ID_conflict=True
+                   elif d in word1 and d in word2:
+                       mapping_Dic["Where"].append(word2)
+                      # Df_name.append(n)
+                       
                if ID_conflict==True:
                    continue
                if word1 in words_matching_dic_where:
@@ -166,7 +186,11 @@ if where_exist:
 
                   
        for k in  words_matching_dic_where:
-            mapping_Dic["Where"].append(words_matching_dic_where[k][0])   
+            mapping_Dic["Where"].append(words_matching_dic_where[k][0])
+                #Df_name.append(n)
+           
+            
+    
             
 
 
@@ -185,7 +209,8 @@ if where_exist:
                if d > 0.91:
                    print(d,word1,word2)              
                    mapping_Dic["Where"].append(word2)
-                   words_matching_dic_where[word1]=[word2,d] 
+                   words_matching_dic_where[word1]=[word2,d]
+                  # Df_name.append(n)
 #remove duplicate from where attributes 
 
     mapping_Dic["Where"] = list(dict.fromkeys(mapping_Dic["Where"]))
@@ -219,11 +244,11 @@ if where_exist:
 
 ###matching words with attributes dictionaries for select statment 
 words_matching_dic_select={}
-select_All=["all"]
+select_All=[" all data ","all info","all information"]
 selectall=False
 for n in DF_att:## iterating on tables names 
     for j in select_All:
-       if j in filtered_sentence_select:
+       if  j in Select_sent:
          mapping_Dic["Select"].append("*")
          selectall=True
     if selectall ==True:
@@ -241,8 +266,8 @@ for n in DF_att:## iterating on tables names
                 if e in filtered_sentence_select:
                      filtered_sentence_select.remove(e)
                 
-    if select_attribute_exist ==True:
-        continue
+        if select_attribute_exist ==True:
+              continue
     
     att_Dict_select={} 
     
@@ -255,12 +280,18 @@ for n in DF_att:## iterating on tables names
         Token_Sort_Ratio = fuzz.token_sort_ratio(word1,word2)
         Token_Set_Ratio = fuzz.token_set_ratio(word1,word2)
         if Ratio > 65:
+            
               print(  word1 +" matches with attribute  "+ word2+" with ratio " + str(Ratio) + " in the select clause")
               list_attributes_matched.append(word2)
-              ID_list=["ID","_ID"]
+              ID_list=["id"]
               ID_conflict=False 
-              if "ID" in word2 and "ID" not in word1:
-                     ID_conflict=True
+              for d in ID_list:
+                  if d in word2 and d not in word1:
+                       ID_conflict=True
+                  
+                  #elif d in word1 and d in word2:
+                      # mapping_Dic["Select"].append(word2)
+                     #  print("hi" + word2)
               if ID_conflict==True:
                    continue
               if word1 in words_matching_dic_select:
@@ -281,6 +312,8 @@ for n in DF_att:## iterating on tables names
                      
     for k in  words_matching_dic_select:
         mapping_Dic["Select"].append(words_matching_dic_select[k][0])
+        if k in filtered_sentence_select:
+              filtered_sentence_select.remove(k)
        
         
 
@@ -296,6 +329,12 @@ for n in DF_att:## iterating on tables names
                if d > 0.91:
                   print(d,word1,word2)
                   mapping_Dic["Select"].append(word2)
+                #  Df_name.append(n)
+                  if word1 in filtered_sentence_select:
+                     filtered_sentence_select.remove(word1)
+
+                      
+                  
                        
 #remove duplicate from Select attributes 
 
@@ -315,13 +354,24 @@ for h in filtered_sentence_select:
             if h in words_matching_dic_select:
                 filtered_sentence_select.remove(h)
         
-agg_Dict={"min":"min","max":"max","minimumm":"min","maximum":"max","total number":"count","number":"count","total":"sum","count":"count","least":"min","highest":"max","sum":"sum","mean":"Avg","average":"avg","lowest":"min"}
+agg_Dict={"min":"min"," max ":"max","minimumm":"min","maximum":"max","total number":"count","number":"count","total":"sum","count":"count","least":"min","highest":"max","sum":"sum","mean":"Avg","average":"avg","lowest":"min","avg":"avg"}
 for i in agg_Dict:
-    if i in Select_sent:
-         mapping_Dic["AGG"].append(agg_Dict[i])
+    if i in example_sent:
+         splitted_AGG=i.split(" ")
+         if filtered_sentence_select!=[]:
+             mapping_Dic["AGG"].append(agg_Dict[i])
+         for e in splitted_AGG:
+             if e in filtered_sentence_select:
+                  filtered_sentence_select.remove(e)
+             if e in filtered_sentence_where:
+                   filtered_sentence_where.remove(e)
+                   
+            
+
+         
 
         
-operator_Dic={"with names":"=","named":"=","before":"<","after":">","from":">","since":">","greater than":">","less than" : "<","more than": ">","lower than":"<","equal":"=","equals":"=","=":"=","is":"=","in":"=","are":"="}
+operator_Dic={"with names":"=","named":"=","before":"<","after":">","since":">","greater than":">","less than" : "<","more than": ">","lower than":"<","equal":"=","equals":"=","=":"=","is":"="," in ":"=","are":"=","during":"="}
 
 for i in operator_Dic:
     if i in example_sent:      
@@ -332,23 +382,33 @@ for i in operator_Dic:
             for e in splitted_operator:
                if where_exist:
                   if e in filtered_sentence_where:
-                      filtered_sentence_where.remove(e) 
-               else:
+                     filtered_sentence_where.remove(e) 
+               elif e in filtered_sentence_select:
                    filtered_sentence_select.remove(e)
-                      
+
+Ranges_Dic=["between" , "from"]
+for i in Ranges_Dic:
+    if i in example_sent:
+        if i == "between":
+            if "and" in Where_sent:
+                mapping_Dic["between"]=filtered_sentence_where
+        elif i == "from":
+            if "to" in Where_sent:
+                mapping_Dic["between"]=filtered_sentence_where     
+        
+        
+                   
                       
 if where_exist:
-    
+
      mapping_Dic["op_values"]=filtered_sentence_where
 else:
     mapping_Dic["op_values"]=filtered_sentence_select
 
-    
-if mapping_Dic["Where"]==[]:
-    if mapping_Dic["op_values"] != []:
-        if mapping_Dic["operator"] != []:
-         mapping_Dic["Where"]=mapping_Dic["Select"]   
-         
+print(mapping_Dic["Where"])
+
+  
+      
 if mapping_Dic["operator"]==[]:
    if mapping_Dic["Where"]!=[]:
      if mapping_Dic["op_values"] != []:
@@ -359,9 +419,97 @@ if mapping_Dic["Select"]==[]:
      if mapping_Dic["op_values"] != []:
        if mapping_Dic["operator"]!=[]:    
            mapping_Dic["Select"]=mapping_Dic["Where"]   
+           
+if mapping_Dic["Select"]!=[]:
+   if mapping_Dic["Where"]!=[]:
+     if mapping_Dic["op_values"] == []:
+       if mapping_Dic["operator"]==[]:
+           if mapping_Dic["between"]==[]:
+                mapping_Dic["Select"].append(mapping_Dic["Where"][0])
+                mapping_Dic["Where"]=[]
+           
+if mapping_Dic["Select"]!=[]:
+   if mapping_Dic["Where"]==[]:
+       if mapping_Dic["operator"]==[]:
+               mapping_Dic["op_values"] = []
+# searching for years in my sentence 
+#if mapping_Dic["between"]!=[]:           
+   # if search_dates(mapping_Dic["between"][0]):
+    #      mapping_Dic["Where"]=["year"]
+
+## checking on numbers
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+
+if mapping_Dic["op_values"]!=[]:
+   # if len(mapping_Dic["op_values"])!=len(mapping_Dic["Where"]):
+        for x in mapping_Dic["op_values"]:
+              if search_dates(x): 
+                  if hasNumbers(x):
+                      y = int(x)
+                      if y > 1950 and y < 2020:
+                          mapping_Dic["Where"]=["year"]
+                   #mapping_Dic["Where"].append("year")
+                      
+                   
+if mapping_Dic["between"]!=[]:
+        for x in mapping_Dic["between"]:
+              if search_dates(x):  
+                  if hasNumbers(x):
+                      y = int(x)
+                      if y > 1950 and y <2020:
+                          mapping_Dic["Where"]=["year"]
+                          
+                      
+                
+if mapping_Dic["Where"]==[]:
+    if mapping_Dic["op_values"] != []:
+        if mapping_Dic["operator"] != []:
+            mapping_Dic["Where"]=mapping_Dic["Select"]    
+            
+if len(mapping_Dic["op_values"])>len(mapping_Dic["Where"]):
+    if  "and" not in Where_sent:
+        #m=len(mapping_Dic["op_values"])-len(mapping_Dic["Where"]) 
+        mapping_Dic["op_values"]=[' '.join(mapping_Dic["op_values"])]
+        
+        
+for r in mapping_Dic["Select"]:
+    for n in DF_att:
+        if r in DF_att[n]:
+            Df_name.append(n)
+            
+for r in mapping_Dic["Where"]:
+    for n in DF_att:
+        if r in DF_att[n]:
+            Df_name.append(n)            
+    
+    
+    
+    
+Df_name=list(dict.fromkeys(Df_name))   
+if len(Df_name) > 1:
+    print ("error: attributes are from different tables" )
+
+
+## generating the query
+if mapping_Dic["Select"] != [] and len(Df_name) == 1:
+    if mapping_Dic["AGG"] != []:
+        output_query= Df_name[0] + ".selectExpr('"  + mapping_Dic["AGG"][0] +"("+ mapping_Dic["Select"][0] + ")')"
+    else:
+        output_query= Df_name[0] + ".select('"  + mapping_Dic["Select"][0]
+        for c in range(len(mapping_Dic["Select"])):
+            if c !=0:
+                output_query= output_query + "','"  + mapping_Dic["Select"][c] 
+        output_query=output_query+"')"
+    if mapping_Dic["between"]!=[]:
+         output_query=output_query + ".where('" + mapping_Dic["Where"][0]+"').between("+ mapping_Dic["between"][0]+","+ mapping_Dic["between"][1] +")"
+    if mapping_Dic["Where"]!=[] and mapping_Dic["between"]==[]:
+        output_query=output_query + ".where('" + mapping_Dic["Where"][0]+"'"+ mapping_Dic["operator"][0] +"'"+mapping_Dic["op_values"][0]+"')"
+        
     
 
-#remove duplicate from attributes list 
-
 print("output info are:")
+ 
 print(mapping_Dic)
+print(output_query)
